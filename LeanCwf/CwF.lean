aec36a3dd7a43a91baaa75e5c1716fae91c9a88e
Joey Eremondi <joey@eremondi.com>
2023-10-25 13:12:38 +0100

    Add more documentation, cleanup

--- LeanCwf/CwF.lean
+++ LeanCwf/CwF.lean
@@ -8,30 +8,38 @@ open CategoryTheory
 universe u v u2
 
 
-
   -- Terms and Types in a CwF, without the comprehension structure
+  -- A CwF over C has a Fam-valued presheaf
+  -- We interpret objects of C as contexts
   class TmTy (C : Type u) [Category.{v} C] : Type ((max v u+1)+1) where
     F : Functor Cᵒᵖ Fam.{u}
 
   section
   variable {C : Type u} [Category.{v}  C] [TmTy.{u,v} C]
 
+  -- The index set of the functor F gives types over a given context
   def Ty (Γ : C) : Type u :=  ixSet (TmTy.F.obj (Opposite.op Γ))
 
+  -- The family for a given context and type gives the set of
+  -- terms of that type
   def Tm {Γ : C} (T : Ty Γ) : Type u := famFor (TmTy.F.obj (Opposite.op Γ)) T
 
-  -- Definition of substitution and
-  -- We'll overload these with a smart type-directed notation later
-  --
+  -- Definition of substitution for types
+  -- Any C-arrow can be lifted to a substitution function on types
+  -- by the functoral structure of F.
   def tySub {Δ Γ: C} (T : Ty Δ) (θ : Γ ⟶ Δ) : Ty Γ :=
     mapIx (TmTy.F.map θ.op) T
 
+  -- Notation for substitution on types
   notation T "⦃" θ "⦄"  => tySub T θ
 
+  -- Definition fo substitution on terms
+  -- Like for types, but the resulting term also has the substitution applied
+  -- in its type
   def tmSub  {Γ Δ : C} {T : Ty Δ} ( t : Tm T )  (θ : Γ ⟶ Δ) : Tm (T⦃θ⦄) :=
     mapFam (TmTy.F.map θ.op) t
 
-
+  -- Notation for substitution on terms
   notation t "⦃" θ "⦄ₜ"  => tmSub t θ
 
   -- Subsitution by the identity has no effect
@@ -44,12 +52,6 @@ universe u v u2
   theorem tySubComp {Γ Δ Ξ : C} {T : Ty Γ} {f : Δ ⟶ Γ} {g : Ξ ⟶ Δ} : T⦃g ≫ f⦄ = (T⦃f⦄)⦃g⦄   := by
     simp [tySub]
 
-
-  -- instance coeId {Γ : C} {T : Ty Γ} : Coe (Tm (T⦃𝟙 Γ⦄)) (Tm T) where
-  --   coe := cast (by simp [tySubId])
-
-  -- instance coeComp {Γ Δ Ξ : C} {T : Ty Γ} {f : Δ ⟶ Γ} {g : Ξ ⟶ Δ} : Coe (Tm (T⦃g ≫ f⦄)) (Tm ((T⦃f⦄)⦃g⦄)) where
-  --   coe t := cast (by simp [tySubComp]) t
 
   -- Same laws but for substitution on terms instead of types
   @[simp]
@@ -64,34 +66,57 @@ universe u v u2
     rw [TmTy.F.map_comp]
     apply mapFamComp
 
-  theorem tmEq {Γ : C} {S T : Ty Γ} (eq : S = T ) : Tm S = Tm T := by aesop
+  -- -- Helpful lemma: equal types have equal sets of terms
+  -- theorem tmEq {Γ : C} {S T : Ty Γ} (eq : S = T ) : Tm S = Tm T := by aesop
 
 
--- A CwF has a type-term structure, plus context-extension and an initial object
+-- A CwF has a type-term structure,
+-- plus context-extension, substitution externsn, and an initial object
 class CwF (C : Type u) [Category.{v} C] [TmTy C] : Type _ where
+  -- Context extension
   snoc : (Γ : C) → Ty Γ → C
-  --The projection substitution. Applying this weakens a type/term
+  --The projection substitution
+  --Applying this weakens a type/term
   --by introducing an unused variable
   p : {Γ : C} → {T : Ty Γ} → snoc Γ T ⟶ Γ
   --The variable introduced by extending a context
   v : {Γ : C} → {T : Ty Γ} → Tm (T⦃p⦄ : Ty (snoc Γ T))
   -- Every morphism can be extended to extended contexts
+  -- This basically says "do whatever f does, and replace the newly introduced variable with t"
   ext : {Γ Δ : C} → {T : Ty Γ} → (f : Δ ⟶ Γ) → (t : Tm (T⦃f⦄)) → Δ ⟶ snoc Γ T
-  -- hExt : {Γ Δ : C} → {T : Ty Γ} → {S : Ty Δ} → (f : Δ ⟶ Γ) → {{eq : S = T⦃f⦄}} → (t : Tm S) → Δ ⟶ snoc Γ T
   -- Such an extension is the unique morphism satisfying certain laws
-  ext_p : {Γ Δ : C} → {T : Ty Γ} → (f : Δ ⟶ Γ) → (t : Tm (tySub T f)) → (ext f t) ≫ p = f
+  -- Extending and composing with p cancels: if you introduce an unused variable then replace it with t,
+  -- you get the original substitution
+  ext_p : {Γ Δ : C} → {T : Ty Γ}
+    → (f : Δ ⟶ Γ) → (t : Tm (tySub T f))
+    → (ext f t) ≫ p = f
 
+  --An extended substitution, applied to the newly generated variable, produces
+  --the term by which the subsitution was extended
   ext_v : {Γ Δ : C} → {T : Ty Γ} → (f : Δ ⟶ Γ) → (t : Tm (tySub T f)) → HEq (v⦃ext f t⦄ₜ) t
   -- The extension is unique
   -- ext_unique : {Γ Δ : C} → {T : Ty Γ} → (f : Δ ⟶ Γ) → (t : Tm (tySub T f)) → (g : _) → g ≫ p = f → HEq (v⦃g⦄ₜ) t → g = ext f t
-  ext_nat : {Γ Δ Ξ : C} → {T : Ty Γ}  → (f : Δ ⟶ Γ) → (g : Ξ ⟶ Δ) → (t : Tm (T⦃f⦄)) → (g ≫ ext f t) = (ext (g ≫ f) (cast (symm (congrArg Tm tySubComp)) (t⦃g⦄ₜ)))
+
+  -- If you compose with an extension, this is the same as extending by the composition,
+  -- except that you also end up substituting in the term you're extending by.
+  -- Unfortunate ugliness due to the fact that Tm⦃g ≫ f⦄ is not definitionally equal to tm⦃f⦄⦃g⦄
+  ext_nat : {Γ Δ Ξ : C} → {T : Ty Γ}
+    → (f : Δ ⟶ Γ)
+    → (g : Ξ ⟶ Δ)
+    → (t : Tm (T⦃f⦄))
+    → (g ≫ ext f t) = (ext (g ≫ f) (cast (symm (congrArg Tm tySubComp)) (t⦃g⦄ₜ)))
+  -- If you take a weaning and extend it with the newly introduced variable, you get the identity,
+  -- because it just replaces each v with v
   ext_id : {Γ : C} → {T : Ty Γ} → ext p v = 𝟙 (snoc Γ T)
 
 notation Γ "⬝" T => CwF.snoc Γ T
 notation "⟪" θ "," t "⟫" => CwF.ext θ t
 
 
--- theorem ext_unique {Γ Δ : C} [inst : CwF C] {T : Ty Γ} (f : Δ ⟶ Γ) (t : Tm (T⦃f⦄)) (g : Δ ⟶ Γ ⬝ T) (pfComp : f = (g ≫ CwF.p)) (pfv : t (cast (tmEq pfComp) CwF.v⦃g⦄ₜ) ) : g = ⟪f,t⟫ := by
+theorem ext_unique {Γ Δ : C} [inst : CwF C] {T : Ty Γ}
+  (f : Δ ⟶ Γ) (t : Tm (T⦃f⦄)) (g : Δ ⟶ Γ ⬝ T)
+  (pfComp : f = (g ≫ CwF.p)) (pfv : t (cast (tmEq pfComp) CwF.v⦃g⦄ₜ) )
+  : g = ⟪f,t⟫ := by
 --   cases (pfComp) with
 --   | refl =>
 --     aesop
--- LeanCwf/Fam.lean
+++ LeanCwf/Fam.lean
@@ -12,59 +12,28 @@ universe  u
 abbrev Fam : Type (u + 1) := Arrow (Type u)
 
 
-
+-- We can make a family from any type and something indexed by this type
 def mkFam (A : Type u) (B : A → Type u) :  Fam :=
   { left := Σ x : A , B x
     right := A
     hom := Sigma.fst }
 
+--Projection to get the index type
 def ixSet (arr : Fam) : Type u :=
   arr.right
 
+--Projection to get the indexed type
 def famFor (arr : Fam) (a : ixSet arr) : Type u :=
   {ab : arr.left // arr.hom ab = a}
 
+--The index projection cancels with the constructor
 theorem famForIxInv (A : Type u) (B : A → Type u) : ixSet (mkFam A B) = A := rfl
 
-def mapIx  {AB₁ AB₂ : Fam}  (f : AB₁ ⟶ AB₂) : ixSet AB₁ → ixSet AB₂ := f.right
-
-@[simp]
-theorem mapIxId {AB : Fam} {x : ixSet AB} : mapIx (𝟙 AB) x = x := by
-  rfl
-
-@[simp]
-theorem mapIxComp  {AB₁ AB₂ AB₃ : Fam}  (f : AB₁ ⟶ AB₂) (g : AB₂ ⟶ AB₃) : mapIx (f ≫ g) = mapIx g ∘ mapIx f := by rfl
-
-def mapFam {AB₁ AB₂ : Fam}  (f : AB₁ ⟶ AB₂) {a : ixSet AB₁} (b : famFor AB₁ a ) : famFor AB₂ (mapIx f a) :=
-  ⟨f.left b.val, by
-    cases b with
-    | mk ab abEq =>
-      simp [<- abEq, mapIx]
-      apply (congrFun f.w ab)
-  ⟩
-  -- cases b with
-  -- | mk ab abEq =>
-  --   rw [<- abEq]
-  --   exact ⟨f.left ab, congrFun f.w ab⟩
-
-
-@[simp]
-def mapFamId {AB : Fam}  {a : ixSet AB} (b : famFor AB a ) : mapFam (𝟙 AB) b = b := by
-  cases b with
-  | mk ab abEq =>
-    aesop_subst [abEq]
-    simp only [mapIxId, Functor.id_obj]
-    rfl
-
-@[simp]
-theorem mapFamComp  {AB₁ AB₂ AB₃ : Fam}  (f : AB₁ ⟶ AB₂) (g : AB₂ ⟶ AB₃) {a : ixSet AB₁} (b : famFor AB₁ a)
-  : HEq (mapFam (f ≫ g) b) (mapFam g (mapFam f b)) := by
-    simp [mapFam, <- b.property]
-
-
-
+-- For any index, the family projection cancels with the constructor,
+-- up to isomorphism
 @[aesop safe]
-theorem famForInv (A : Type u) (B : A → Type u) (a : A) : famFor (mkFam A B) a ≅  B a where
+theorem famForInv (A : Type u) (B : A → Type u) (a : A)
+  : famFor (mkFam A B) a ≅ B a where
   hom := fun
     | .mk ab eq => by
       rw [<- eq]
@@ -85,7 +54,8 @@ theorem famForInv (A : Type u) (B : A → Type u) (a : A) : famFor (mkFam A B) a
     reduce
     rfl
 
-
+-- For any family, making a new family from the projections
+-- of that family prodices the original family, up to isomorphism
 @[aesop safe]
 theorem mkFamInv (arr : Fam ) : mkFam (ixSet arr) (famFor arr) ≅ arr where
   hom := by
@@ -128,6 +98,46 @@ theorem mkFamInv (arr : Fam ) : mkFam (ixSet arr) (famFor arr) ≅ arr where
     | mk B A hom =>
       reduce
       congr
+
+
+--A Fam arrow gives us a function on indices
+def mapIx  {AB₁ AB₂ : Fam}  (f : AB₁ ⟶ AB₂) : ixSet AB₁ → ixSet AB₂ := f.right
+
+-- Mapping the identity morphism over a family's index is the identity
+@[simp]
+theorem mapIxId {AB : Fam} {x : ixSet AB} : mapIx (𝟙 AB) x = x := by
+  rfl
+
+-- Mapping a composite arrow over an index gives the composite
+@[simp]
+theorem mapIxComp  {AB₁ AB₂ AB₃ : Fam}  (f : AB₁ ⟶ AB₂) (g : AB₂ ⟶ AB₃) : mapIx (f ≫ g) = mapIx g ∘ mapIx f := by rfl
+
+-- For an index a, a Fam arrow gives a function from the domain family over index a
+-- to the codomain family, whose index is the arrow mapped over a
+def mapFam {AB₁ AB₂ : Fam}  (f : AB₁ ⟶ AB₂) {a : ixSet AB₁} (b : famFor AB₁ a ) : famFor AB₂ (mapIx f a) :=
+  ⟨f.left b.val, by
+    cases b with
+    | mk ab abEq =>
+      simp [<- abEq, mapIx]
+      apply (congrFun f.w ab)
+  ⟩
+
+-- Mapping the idenitity over an element of a family is the identity
+@[simp]
+def mapFamId {AB : Fam}  {a : ixSet AB} (b : famFor AB a ) : mapFam (𝟙 AB) b = b := by
+  cases b with
+  | mk ab abEq =>
+    aesop_subst [abEq]
+    simp only [mapIxId, Functor.id_obj]
+    rfl
+
+-- Mapping a composite arrow over an element of a family
+-- is the composition of the mappings
+@[simp]
+theorem mapFamComp {AB₁ AB₂ AB₃ : Fam}  (f : AB₁ ⟶ AB₂) (g : AB₂ ⟶ AB₃) {a : ixSet AB₁} (b : famFor AB₁ a)
+  : HEq (mapFam (f ≫ g) b) (mapFam g (mapFam f b)) := by
+    simp [mapFam, <- b.property]
+
 
 
   -- Every family for a given index type is equivalent to the slice over that type
